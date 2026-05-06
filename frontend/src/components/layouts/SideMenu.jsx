758cd1dab810cc3bc85785f54e257415b38f08b9
Avinash Kumar <avinash.kumar@boostb2b.com>
2026-05-06 21:29:56 +0530

    Fixed Logout Route

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -18,7 +18,7 @@ const SideMenu = ({ activeMenu }) => {
   const handleLogout = () => {
     localStorage.clear();
     clearUser();
-    navigate("/login");
+    window.location.replace("/Expense-Tracker/#/login");
   };
 
   return (
